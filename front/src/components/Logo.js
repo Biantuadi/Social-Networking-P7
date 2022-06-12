f9e93062323d52b374c1f5bd258ea6053f74ad40
Kevin-beni <benibiantuadi@gmail.com>
2022-06-12 02:45:27 +0200

    first commit mise en place de réacte

---
+++ front/src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './icon-left-font-monochrome-black.png';
+
+const Logo = () => {
+    return (
+        <div className='logo'>
+            <img src={logo} alt="logo_groupomania" />
+        </div>
+    );
+};
+
+export default Logo;
