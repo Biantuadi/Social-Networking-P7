f91e79c88c9c65c7527f764f862bcea8756f5e4d
Kevin-beni <benibiantuadi@gmail.com>
2022-06-16 16:14:25 +0200

    api login créé à 100%

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,18 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
 
 exports.signup = (req, res) => {
-    res.send('signup');
-}
+    bcrypt.hash(req.body.password, 10)
+    .then(hash =>{
+        const user = new User({ 
+            email: req.body.email,
+            password: hash
+        })
+        user.save()
+        .then((res) => res.status(201).json({message: 'Utilisateur créé !'}))
+        .catch(err => res.status(400).json({error: err}))
+    })
+    .catch(err => res.status(500).json({error: err}))
+
+} 
