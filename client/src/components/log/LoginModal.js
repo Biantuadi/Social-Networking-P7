40a479d77bccba0c2446dd6b4f39f810f2bdfc80
Kevin-beni <benibiantuadi@gmail.com>
2022-06-17 19:42:33 +0200

    réalisation login 80%

--- client/src/components/log/LoginModal.js
+++ client/src/components/log/LoginModal.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 
 const LoginModal = () => {
-    return (
-        <main>
-            <h1>Connexion</h1>
-        </main>
-    );
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleLogin = (e) => {};
+
+  return (
+    <main>
+      <h1>Connexion</h1>
+      <form
+        action=""
+        onSubmit={handleLogin}
+        id="Login-form"
+        className="form__auth"
+      >
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="password">Mot de passe</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+
+        <input type="submit" value="Se connecter" className="btn-auth" />
+      </form>
+    </main>
+  );
 };
 
 export default LoginModal;
--- client/src/components/log/SignupModal.js
+++ client/src/components/log/SignupModal.js
@@ -1,9 +1,24 @@
 import React from "react";
+import axios from "axios";
 
 const SignupModal = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleSignup = (e) => {};
+
   return (
-    <main>
+    <main className="main__Auth">
       <h1>Inscription</h1>
+      <form action="" onSubmit={handleSignup} id="signup-form">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </form>
     </main>
   );
 };
