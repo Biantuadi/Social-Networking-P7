9b0784d677c4844e901692f59fb769c800b54815
Kevin-beni <benibiantuadi@gmail.com>
2022-06-22 19:05:57 +0200

    page home realisé 70% witouh axios in front

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import iconUser from "../img/icon/user-solid.svg";
-import iconLogout from "../img/icon/arrow-right-from-bracket-solid.svg";
 import {NavLink} from 'react-router-dom';
 import Logout from './log/Logout';
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,6 @@ import Nav from "../components/Nav";
 import imgTest from "../img/test.jpg";
 import axios from "axios";
 
-
 const Home = () => {
   const uid = React.useContext(UidContext);
 
@@ -24,6 +23,18 @@ const Home = () => {
 
   // }
 
+  const changeHeart = () => {
+    const heart = document.querySelector(".fa-heart");
+    heart.classList.toggle("fa-regular");
+    heart.classList.toggle("fa-solid");
+
+    let likeAudio = new Audio(
+      "https://phoneky.co.uk/content/mp3tones/tone/2020/sound-fx/facebookpop_c01368ea5038615.mp3"
+    );
+
+    if (heart.classList.contains("fa-solid")) likeAudio.play();
+  };
+
   return (
     <>
       {uid ? (
@@ -34,29 +45,40 @@ const Home = () => {
           </header>
 
           <main className="mainContainer__post">
-            
-
             <div className="friend_Post_No_Img ">
-                <div className="nameAndimg">
+              <div className="container_name_img_time_update">
+                <div className="img-name-time">
                   <img src={imgTest} alt="" />
+                  <div className="nameAndT">
                   <h4>Kevin02</h4>
+                  <span>12 : 24</span>
+                  </div>
                 </div>
 
-                <div className="viewPost">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Excepturi, esse!
-                  </p>
-                  <br />
-                  <img src="https://theatrum-belli.com/wp-content/uploads/2019/03/Soldat-avec-syst%C3%A8me-FELIN.jpg" alt="" />
-                </div>
+                <div className="update">
+                  <i class="fa-solid fa-pen-to-square"></i>
+                  <i class="fa-solid fa-trash-can"></i>
 
-                <div className="iconImgAndPost">
-                  <i className="fa-solid fa-heart"></i>
-                  <i className="fa-solid fa-comment"></i>
                 </div>
               </div>
-              <br />
+
+              <div className="viewPost">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Excepturi, esse! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum, exercitationem ratione, qui illo ab libero impedit culpa aspernatur fugiat earum recusandae amet consequatur optio dolorum sequi voluptates minus debitis aut.
+                </p>
+                <img
+                  src="https://theatrum-belli.com/wp-content/uploads/2019/03/Soldat-avec-syst%C3%A8me-FELIN.jpg"
+                  alt=""
+                />
+              </div>
+
+              <div className="iconImgAndPost">
+                <i className="fa-regular fa-heart" onClick={changeHeart}></i>
+                <i className="fa-regular fa-comment"></i>
+              </div>
+            </div>
+            <br />
           </main>
         </div>
       ) : (
