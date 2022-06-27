3b149abd90d5cd9ac6ec6330f98e9bfb3cc80ae1
Kevin-beni <benibiantuadi@gmail.com>
2022-06-27 04:10:44 +0200

    le bon code like comment update post ..

--- client/src/components/Home/DeleteComment.jsx
+++ client/src/components/Home/DeleteComment.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteComment } from "../../actions/post.action";
 
-const DeleteComment = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const DeleteComment = ({ comment, postId }) => {
+  const [isAuthor, setIsAuthor] = React.useState(false);
+  const [delet, setDelete] = React.useState(false);
+  const userId = localStorage.getItem("uid");
+  const dispatch = useDispatch();
+
+//   const handleDelete = () => dispatch(deleteComment(postId, comment._id));
+
+  useEffect(() => {
+    const checkAuthor = () => {
+      if (userId === comment.commenterId) {
+        setIsAuthor(true);
+      }
+    };
+    checkAuthor();
+  }, [userId, comment.commenterId]);
+
+  return (
+    <>
+      {isAuthor && (
+        <i
+          className="fas fa-trash-alt"
+          onClick={() => {
+            if (
+              window.confirm(
+                "vous êtes sur le point de supprimer ce commentaire"
+              )
+            ) {
+            //   handleDelete();
+            }
+          }}
+        ></i>
+      )}
+    </>
+  );
 };
 
 export default DeleteComment;
